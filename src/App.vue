54ae8bb65f2862fa9f83ae26d5828737c61a81a7
Matthias Jofre <matthias.jofre@gmail.com>
2022-09-05 22:38:39 -0400

    update 3 files and create 1 file

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MyCounter @selection="checkAnswer"></MyCounter>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MyCounter from './components/MyCounter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MyCounter
+},
+methods: {
+    checkAnswer(num) {
+      console.log((num))
+    }
   }
 }
 </script>
---
+++ src/components/MyCounter.vue
@@ -0,0 +1,48 @@
+<template>
+  <h1>Soy el componente Contador y tengo el valor de {{ contador }}</h1>
+  <div>
+    <button v-on:click="suma1">Suma +1</button>
+    <button @click="resta1">Resta -1</button>
+    <button @click="$emit('selection', data)">Emit</button>
+    <!-- //https://vuejs-org.translate.goog/guide/components/events.html?_x_tr_sl=en&_x_tr_tl=es&_x_tr_hl=es&_x_tr_pto=sc#defining-custom-events -->
+  </div>
+  <div class="separador">
+    <input v-model="data" type="text" />
+    <h1>{{ data }}</h1>
+    <h1 v-if="contador > 10">Es mayor a 10</h1>
+    <ul id="example-1">
+      <li v-for="(item, index) in items" :key="index.id">
+        {{ item.mensaje }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 0,
+      data: "",
+      items: [],
+    };
+  },
+
+  methods: {
+    suma1() {
+      this.contador++;
+      this.items.push(this.contador);
+      console.log(this.items);
+    },
+    resta1() {
+      this.contador--;
+    },
+  },
+};
+</script>
+
+<style>
+.separador {
+  margin-top: 50px;
+}
+</style>
